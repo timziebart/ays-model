413f3cfc7689fcdd9ba7970cf76a2a147b7e90e8
¨Rebekka¨ <rkoch@physik.hu-berlin.de>
2016-08-01 17:21:23 +0200

    adding examples to the new format and adding titles for plots

--- TechChangeModel.py
+++ TechChangeModel.py
@@ -39,6 +39,36 @@ pi = np.pi
 stylePoint["markersize"] *= 2
 
 
+def techChange_rhs(uB_pB, t, rvar, pBmin, pE, delta, smax, sBmax):
+    uB, pB = uB_pB
+    if sBmax == None:
+        p = pE
+    else:
+        if smax < sBmax * uB:
+            p = pE + smax / uB
+        else:
+            p = sBmax + pE
+
+    duB = rvar * uB * (1 - uB) * (p - pB)
+    dpB = -(pB - pBmin) * ((pB - pBmin) * uB - delta)
+
+    return np.array([duB, dpB])
+
+def techChange_sunny(p):
+    """sunny constraint for techChangeModel"""
+    return p[:, 0] > 0.325
+
+def techChange_rhsPS(uB_pB, t, rvar, pBmin, pE, delta, smax, sBmax):
+    uB, pB = uB_pB
+    p = np.zeros_like(pB)
+    p[:] = sBmax + pE
+    mask = (smax < sBmax * uB)
+    p[mask] = (pE + smax / uB[mask])
+
+    duB = rvar * uB * (1 - uB) * (p - pB)
+    dpB = -(pB - pBmin) * ((pB - pBmin) * uB - delta)
+    return np.array([duB, dpB])
+
 def evol_noManagement(uB_pB, t=0): # rvar, pBmin,
        # pE, sigmaA, uDedge, pA, delta, smax, sBmax):
     uB, pB = uB_pB
